ff7726858e5e4bbd3cd65e0a12c2ae7028828d23
Priyanshu Kumar <priyanshu@Priyanshus-MacBook-Air.local>
2024-06-21 09:10:14 +0530

    bug fix for mobile nav

--- src/utils/mobileNav.js
+++ src/utils/mobileNav.js
@@ -1,7 +1,7 @@
 const mobileNav = ()=> {
     const headerBtn = document.querySelector(".header__bars");
     const mobileNav = document.querySelector(".mobile-nav");
-    const mobileLinks = document.querySelectorAll(".mobile-nav__links");
+    const mobileLinks = document.querySelectorAll(".mobile-nav__link");
 
     let isMobileNavOpen = false;
 
