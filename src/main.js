0ada6f7c2012ffc23c463ed37e1c4e313c9ae5fb
Priyanshu Kumar <priyanshu@Priyanshus-MacBook-Air.local>
2024-06-21 08:24:22 +0530

    pushing all codes and files

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/mobileNav.css';
+import '../styles/components/line.css';
+import '../styles/components/msgbar.css';
+import '../styles/components/hero.css';
+import '../styles/components/featured.css';
+import '../styles/components/latest.css';
+import '../styles/components/contact.css';
+import '../styles/components/vegetarian.css';
+import '../styles/components/cookbook.css';
+import '../styles/components/fixedBar.css';
+import '../styles/components/footer.css';
+
+
+import '../styles/utils.css';
+
+
+import mobileNav from '../../../portfolio/src/utils/mobile-nav';
+mobileNav();
